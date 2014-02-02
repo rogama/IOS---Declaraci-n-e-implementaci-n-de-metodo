6384d96a6cf473d659c4867bd6f83739d51d2d4f
rober <gascon87@gmail.com>
2014-02-02 21:44:08 +0100

    Initial Commit

---
+++ Metodos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Metodos
+//
+//  Created by rober on 02/02/14.
+//  Copyright (c) 2014 rogama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
