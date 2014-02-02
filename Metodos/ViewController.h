2f001f190872b01be80777e1ce28838b993563cd
rober <gascon87@gmail.com>
2014-02-02 22:05:01 +0100

    Declaracion e implementación de un metido de ejemplo

--- Metodos/ViewController.h
+++ Metodos/ViewController.h
@@ -10,4 +10,23 @@
 
 @interface ViewController : UIViewController
 
+
+//Declaracion
+//empieza con - o con +
+
+//-metodo de objeto
+//+metodo de clase
+
+-(void)nombreDelMetodoConParametro1:(NSString *)parametro1 andParametro2:(NSString *)parametro2;
+
+//nombreDelMetodoConParametro1-> Es el nombre del metodo con una descripcion del Primer parametro
+//(NSString *)-> Tipo del primer parametro
+//parametro1 ->Es el nombre del primer parametro, tal y como lo vamos a usar dentro del metodo
+//andParametro2 -> Le decimos que va a tener un segundo parametro y le damos la descripcion
+//(NSString *)parametro2 -> El tipo y nombre del segundo parametro dentro del metodo
+
+
+
+
+
 @end
--- Metodos/ViewController.m
+++ Metodos/ViewController.m
@@ -17,13 +17,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    
+    //se utiliza self para cosas de nuestra propia clase
+    //Para los metodos Utilizamos los [ ]
+    //En el lugar de los parametros, pasamos los valores necesarios
+    [self nombreDelMetodoConParametro1:@"Texto de Ejemplo1" andParametro2:@"Texto de ejemplo para el segundo ejemplo"];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//Implementacion del metodo
+-(void)nombreDelMetodoConParametro1:(NSString *)parametro1 andParametro2:(NSString *)parametro2{
+    NSLog(@"%@", parametro1);
+    NSLog(@"%@", parametro2);
 }
 
 @end
